f27206be45a31f49bd5c4064bc6cad7a3bea5332
Rob Phoenix <rob@robphoenix.com>
2017-05-30 15:55:22 +0100

    fix tenant create/delete failure

--- aci/tenant.go
+++ aci/tenant.go
@@ -6,7 +6,7 @@ import (
 )
 
 const (
-	tenantsPath     = "/api/mo/uni.json"
+	tenantsPath     = "/api/mo/uni/tn-%s.json"
 	listTenantsPath = "/api/node/class/fvTenant.json"
 )
 
@@ -31,6 +31,11 @@ type TenantsImdata struct {
 	FvTenant `json:"fvTenant"`
 }
 
+// TenantPayload is the data structure used to create a tenant
+type TenantPayload struct {
+	FvTenant `json:"fvTenant"`
+}
+
 // FvTenant ...
 type FvTenant struct {
 	TenantAttributes `json:"attributes"`
@@ -48,6 +53,7 @@ type TenantAttributes struct {
 	NameAlias   string `json:"nameAlias,omitempty"`
 	OwnerKey    string `json:"ownerKey,omitempty"`
 	OwnerTag    string `json:"ownerTag,omitempty"`
+	Rn          string `json:"rn,omitempty"`
 	Status      string `json:"status,omitempty"`
 	UID         string `json:"uid,omitempty"`
 }
@@ -55,11 +61,14 @@ type TenantAttributes struct {
 // editTenant takes a createModify or delete action and performs the
 // necessary API request
 func editTenant(c *Client, t Tenant, action string) error {
-	ta := TenantAttributes{
-		Name:   t.Name,
-		Status: action,
-	}
-	req, err := c.newRequest(http.MethodPost, tenantsPath, ta)
+	tp := TenantPayload{}
+	tp.Dn = "uni/tn-" + t.Name
+	tp.Name = t.Name
+	tp.Rn = "tn-" + t.Name
+	tp.Status = action
+
+	p := fmt.Sprintf(tenantsPath, t.Name)
+	req, err := c.newRequest(http.MethodPost, p, tp)
 	if err != nil {
 		return err
 	}
