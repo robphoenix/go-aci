10c0168a7d770142e90bbd0c7a3c747b94e0530e
Rob Phoenix <rob@robphoenix.com>
2017-05-31 21:48:34 +0100

    add main.go

---
+++ main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"fmt"
+	"log"
+
+	"github.com/robphoenix/go-aci/aci"
+)
+
+func main() {
+	apicURL := "sandboxapicdc.cisco.com"
+	apicUser := "admin"
+	apicPwd := "ciscopsdt"
+
+	apicClient, err := aci.NewClient(apicURL, apicUser, apicPwd)
+	if err != nil {
+		log.Fatal(err)
+	}
+	err = apicClient.Login()
+	if err != nil {
+		log.Fatal(err)
+	}
+	// vrf := aci.VRF{Name: "Jo_Cox", Tenant: "CORBYN"}
+	// err = aci.CreateVRF(apicClient, vrf)
+	// if err != nil {
+	//         log.Fatal(err)
+	// }
+	vrfs, err := aci.ListVRFs(apicClient, "harry")
+	if err != nil {
+		log.Fatal(err)
+	}
+	for _, v := range vrfs {
+		fmt.Printf("v = %+v\n", v)
+	}
+}
