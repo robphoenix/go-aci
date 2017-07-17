d9aba0bd00bbe7f6e64ff5850fb55dc58f44b960
Rob Phoenix <rob@robphoenix.com>
2017-07-17 15:11:58 +0100

    add api calls for add/delete/list methods

--- aci/geolocation.go
+++ aci/geolocation.go
@@ -40,7 +40,6 @@ type Row struct {
 // Rack ...
 type Rack struct {
 	Name string
-	// Nodes
 }
 
 // GeoSiteContainer ...
@@ -126,69 +125,69 @@ type ListResponse struct {
 	Imdata []struct {
 		GeoSite struct {
 			Attributes struct {
-				ChildAction string `json:"childAction"`
-				Descr string `json:"descr"`
-				Dn string `json:"dn"`
-				LcOwn string `json:"lcOwn"`
-				ModTs time.Time `json:"modTs"`
-				Name string `json:"name"`
-				NameAlias string `json:"nameAlias"`
-				OwnerKey string `json:"ownerKey"`
-				OwnerTag string `json:"ownerTag"`
-				Status string `json:"status"`
-				UID string `json:"uid"`
+				ChildAction string    `json:"childAction"`
+				Descr       string    `json:"descr"`
+				Dn          string    `json:"dn"`
+				LcOwn       string    `json:"lcOwn"`
+				ModTs       time.Time `json:"modTs"`
+				Name        string    `json:"name"`
+				NameAlias   string    `json:"nameAlias"`
+				OwnerKey    string    `json:"ownerKey"`
+				OwnerTag    string    `json:"ownerTag"`
+				Status      string    `json:"status"`
+				UID         string    `json:"uid"`
 			} `json:"attributes"`
 			Children []struct {
 				GeoBuilding struct {
 					Attributes struct {
-						ChildAction string `json:"childAction"`
-						Descr string `json:"descr"`
-						LcOwn string `json:"lcOwn"`
-						ModTs time.Time `json:"modTs"`
-						Name string `json:"name"`
-						NameAlias string `json:"nameAlias"`
-						Rn string `json:"rn"`
-						Status string `json:"status"`
-						UID string `json:"uid"`
+						ChildAction string    `json:"childAction"`
+						Descr       string    `json:"descr"`
+						LcOwn       string    `json:"lcOwn"`
+						ModTs       time.Time `json:"modTs"`
+						Name        string    `json:"name"`
+						NameAlias   string    `json:"nameAlias"`
+						Rn          string    `json:"rn"`
+						Status      string    `json:"status"`
+						UID         string    `json:"uid"`
 					} `json:"attributes"`
 					Children []struct {
 						GeoFloor struct {
 							Attributes struct {
-								ChildAction string `json:"childAction"`
-								Descr string `json:"descr"`
-								LcOwn string `json:"lcOwn"`
-								ModTs time.Time `json:"modTs"`
-								Name string `json:"name"`
-								NameAlias string `json:"nameAlias"`
-								Rn string `json:"rn"`
-								Status string `json:"status"`
-								UID string `json:"uid"`
+								ChildAction string    `json:"childAction"`
+								Descr       string    `json:"descr"`
+								LcOwn       string    `json:"lcOwn"`
+								ModTs       time.Time `json:"modTs"`
+								Name        string    `json:"name"`
+								NameAlias   string    `json:"nameAlias"`
+								Rn          string    `json:"rn"`
+								Status      string    `json:"status"`
+								UID         string    `json:"uid"`
 							} `json:"attributes"`
 							Children []struct {
 								GeoRoom struct {
 									Attributes struct {
-										ChildAction string `json:"childAction"`
-										Descr string `json:"descr"`
-										LcOwn string `json:"lcOwn"`
-										ModTs time.Time `json:"modTs"`
-										Name string `json:"name"`
-										NameAlias string `json:"nameAlias"`
-										Rn string `json:"rn"`
-										Status string `json:"status"`
-										UID string `json:"uid"`
+										ChildAction string    `json:"childAction"`
+										Descr       string    `json:"descr"`
+										LcOwn       string    `json:"lcOwn"`
+										ModTs       time.Time `json:"modTs"`
+										Name        string    `json:"name"`
+										NameAlias   string    `json:"nameAlias"`
+										Rn          string    `json:"rn"`
+										Status      string    `json:"status"`
+										UID         string    `json:"uid"`
 									} `json:"attributes"`
 									Children []struct {
 										GeoRack struct {
 											Attributes struct {
-												ChildAction string `json:"childAction"`
-												Descr string `json:"descr"`
-												LcOwn string `json:"lcOwn"`
-												ModTs time.Time `json:"modTs"`
-												Name string `json:"name"`
-												NameAlias string `json:"nameAlias"`
-												Rn string `json:"rn"`
-												Status string `json:"status"`
-												UID string `json:"uid"`
+												ChildAction string    `json:"childAction"`
+												Descr       string    `json:"descr"`
+												LcOwn       string    `json:"lcOwn"`
+												ModTs       time.Time `json:"modTs"`
+												Name        string    `json:"name"`
+												NameAlias   string    `json:"nameAlias"`
+												Rn          string    `json:"rn"`
+												Status      string    `json:"status"`
+												UID         string    `json:"uid"`
 											} `json:"attributes"`
 										} `json:"geoRack"`
 									} `json:"children"`
@@ -201,6 +200,7 @@ type ListResponse struct {
 		} `json:"geoSite"`
 	} `json:"imdata"`
 }
+
 // api/node/mo/uni/fabric/site-%s.json // geo-site name
 // {
 //   "geoSite": {
@@ -274,3 +274,397 @@ type ListResponse struct {
 //   }
 // }
 // response: {"totalCount":"0","imdata":[]}
+
+// // ADD SITE
+// "api/node/mo/uni/fabric/site-%s.json" site name
+// {
+//    "geoSite":{
+//       "attributes":{
+//          "dn":"uni/fabric/site-Site01",
+//          "name":"Site01",
+//          "rn":"site-Site01",
+//          "status":"created"
+//       },
+//       "children":[]
+//    }
+// }
+//
+// // DELETE SITE
+// api/node/mo/uni/fabric.json
+// {
+//   "fabricInst": {
+//     "attributes": {
+//       "dn": "uni/fabric",
+//       "status": "modified"
+//     },
+//     "children": [
+//       {
+//         "geoSite": {
+//           "attributes": {
+//             "dn": "uni/fabric/site-Site01",
+//             "status": "deleted"
+//           },
+//           "children": []
+//         }
+//       }
+//     ]
+//   }
+// }
+//
+// // LIST SITES
+// api/node/class/geoSite.json
+// response:
+// {
+//   "totalCount": "2",
+//   "imdata": [
+//     {
+//       "geoSite": {
+//         "attributes": {
+//           "childAction": "",
+//           "descr": "",
+//           "dn": "uni/fabric/site-default",
+//           "lcOwn": "local",
+//           "modTs": "2017-07-17T08:22:15.217+00:00",
+//           "monPolDn": "uni/fabric/monfab-default",
+//           "name": "default",
+//           "ownerKey": "",
+//           "ownerTag": "",
+//           "status": "",
+//           "uid": "0"
+//         }
+//       }
+//     },
+//     {
+//       "geoSite": {
+//         "attributes": {
+//           "childAction": "",
+//           "descr": "",
+//           "dn": "uni/fabric/site-Site01",
+//           "lcOwn": "local",
+//           "modTs": "2017-07-17T13:54:14.796+00:00",
+//           "monPolDn": "uni/fabric/monfab-default",
+//           "name": "Site01",
+//           "ownerKey": "",
+//           "ownerTag": "",
+//           "status": "",
+//           "uid": "15374"
+//         }
+//       }
+//     }
+//   ]
+// }
+//
+// // ADD BUILDING
+// "api/node/mo/uni/fabric/site-Site01/building-Building02.json"
+// {
+//   "geoBuilding": {
+//     "attributes": {
+//       "dn": "uni/fabric/site-Site01/building-Building02",
+//       "name": "Building02",
+//       "rn": "building-Building02",
+//       "status": "created"
+//     },
+//     "children": []
+//   }
+// }
+//
+// // DELETE BUILDING
+// "api/node/mo/uni/fabric/site-Site01.json"
+// {
+//   "geoSite": {
+//     "attributes": {
+//       "dn": "uni/fabric/site-Site01",
+//       "status": "modified"
+//     },
+//     "children": [
+//       {
+//         "geoBuilding": {
+//           "attributes": {
+//             "dn": "uni/fabric/site-Site01/building-Building01",
+//             "status": "deleted"
+//           },
+//           "children": []
+//         }
+//       }
+//     ]
+//   }
+// }
+//
+// // LIST BUILDINGS
+// "api/node/mo/uni/fabric/site-Site01.json?query-target=children&target-subtree-class=geoBuilding"
+// response:
+// {
+//   "totalCount": "2",
+//   "imdata": [
+//     {
+//       "geoBuilding": {
+//         "attributes": {
+//           "childAction": "",
+//           "descr": "",
+//           "dn": "uni/fabric/site-Site01/building-default",
+//           "lcOwn": "local",
+//           "modTs": "2017-07-17T13:54:14.796+00:00",
+//           "monPolDn": "uni/fabric/monfab-default",
+//           "name": "default",
+//           "status": "",
+//           "uid": "0"
+//         }
+//       }
+//     },
+//     {
+//       "geoBuilding": {
+//         "attributes": {
+//           "childAction": "",
+//           "descr": "",
+//           "dn": "uni/fabric/site-Site01/building-Building01",
+//           "lcOwn": "local",
+//           "modTs": "2017-07-17T13:54:14.796+00:00",
+//           "monPolDn": "uni/fabric/monfab-default",
+//           "name": "Building01",
+//           "status": "",
+//           "uid": "15374"
+//         }
+//       }
+//     }
+//   ]
+// }
+//
+// // ADD FLOOR
+// "api/node/mo/uni/fabric/site-Site01/building-Building01/floor-Floor01.json"
+// {
+//   "geoFloor": {
+//     "attributes": {
+//       "dn": "uni/fabric/site-Site01/building-Building01/floor-Floor01",
+//       "name": "Floor01",
+//       "rn": "floor-Floor01",
+//       "status": "created"
+//     },
+//     "children": []
+//   }
+// }
+//
+// // DELETE FLOOR
+// "api/node/mo/uni/fabric/site-Site01/building-Building01.json"
+// {
+//   "geoBuilding": {
+//     "attributes": {
+//       "dn": "uni/fabric/site-Site01/building-Building01",
+//       "status": "modified"
+//     },
+//     "children": [
+//       {
+//         "geoFloor": {
+//           "attributes": {
+//             "dn": "uni/fabric/site-Site01/building-Building01/floor-Floor01",
+//             "status": "deleted"
+//           },
+//           "children": []
+//         }
+//       }
+//     ]
+//   }
+// }
+//
+// // LIST FLOORS
+// "api/node/mo/uni/fabric/site-Site01/building-Building01.json?query-target=children&target-subtree-class=geoFloor"
+// response:
+// {
+//   "totalCount": "1",
+//   "imdata": [
+//     {
+//       "geoFloor": {
+//         "attributes": {
+//           "childAction": "",
+//           "descr": "",
+//           "dn": "uni/fabric/site-Site01/building-Building01/floor-Floor01",
+//           "lcOwn": "local",
+//           "modTs": "2017-07-17T13:54:14.796+00:00",
+//           "monPolDn": "uni/fabric/monfab-default",
+//           "name": "Floor01",
+//           "status": "",
+//           "uid": "15374"
+//         }
+//       }
+//     }
+//   ]
+// }
+//
+// // ADD ROOM
+// "api/node/mo/uni/fabric/site-Site01/building-Building01/floor-Floor01/room-Room01.json"
+// {
+//   "geoRoom": {
+//     "attributes": {
+//       "dn": "uni/fabric/site-Site01/building-Building01/floor-Floor01/room-Room01",
+//       "name": "Room01",
+//       "rn": "room-Room01",
+//       "status": "created"
+//     },
+//     "children": []
+//   }
+// }
+//
+// // DELETE ROOM
+// "api/node/mo/uni/fabric/site-Site01/building-Building01/floor-Floor01.json"
+// {
+//   "geoFloor": {
+//     "attributes": {
+//       "dn": "uni/fabric/site-Site01/building-Building01/floor-Floor01",
+//       "status": "modified"
+//     },
+//     "children": [
+//       {
+//         "geoRoom": {
+//           "attributes": {
+//             "dn": "uni/fabric/site-Site01/building-Building01/floor-Floor01/room-Room01",
+//             "status": "deleted"
+//           },
+//           "children": []
+//         }
+//       }
+//     ]
+//   }
+// }
+//
+// LIST ROOMS
+// "api/node/mo/uni/fabric/site-Site01/building-Building01/floor-Floor01.json?query-target=children&target-subtree-class=geoRoom"
+// response:
+// {
+//   "totalCount": "1",
+//   "imdata": [
+//     {
+//       "geoRoom": {
+//         "attributes": {
+//           "childAction": "",
+//           "descr": "",
+//           "dn": "uni/fabric/site-Site01/building-Building01/floor-Floor01/room-Room01",
+//           "lcOwn": "local",
+//           "modTs": "2017-07-17T13:54:14.796+00:00",
+//           "monPolDn": "uni/fabric/monfab-default",
+//           "name": "Room01",
+//           "status": "",
+//           "uid": "15374"
+//         }
+//       }
+//     }
+//   ]
+// }
+//
+// // ADD ROW
+// "api/node/mo/uni/fabric/site-Site01/building-Building01/floor-Floor01/room-Room01/row-Row01.json"
+// {
+//   "geoRow": {
+//     "attributes": {
+//       "dn": "uni/fabric/site-Site01/building-Building01/floor-Floor01/room-Room01/row-Row01",
+//       "name": "Row01",
+//       "rn": "row-Row01",
+//       "status": "created"
+//     },
+//     "children": []
+//   }
+// }
+//
+// // DELETE ROW
+// "api/node/mo/uni/fabric/site-Site01/building-Building01/floor-Floor01/room-Room01.json"
+// {
+//   "geoRoom": {
+//     "attributes": {
+//       "dn": "uni/fabric/site-Site01/building-Building01/floor-Floor01/room-Room01",
+//       "status": "modified"
+//     },
+//     "children": [
+//       {
+//         "geoRow": {
+//           "attributes": {
+//             "dn": "uni/fabric/site-Site01/building-Building01/floor-Floor01/room-Room01/row-Row01",
+//             "status": "deleted"
+//           },
+//           "children": []
+//         }
+//       }
+//     ]
+//   }
+// }
+//
+// LIST ROWS
+// "api/node/mo/uni/fabric/site-Site01/building-Building01/floor-Floor01/room-Room01.json?query-target=children&target-subtree-class=geoRow"
+// response:
+// {
+//   "totalCount": "1",
+//   "imdata": [
+//     {
+//       "geoRow": {
+//         "attributes": {
+//           "childAction": "",
+//           "descr": "",
+//           "dn": "uni/fabric/site-Site01/building-Building01/floor-Floor01/room-Room01/row-Row01",
+//           "lcOwn": "local",
+//           "modTs": "2017-07-17T13:54:14.796+00:00",
+//           "monPolDn": "uni/fabric/monfab-default",
+//           "name": "Row01",
+//           "status": "",
+//           "uid": "15374"
+//         }
+//       }
+//     }
+//   ]
+// }
+//
+// // ADD RACK
+// "api/node/mo/uni/fabric/site-Site01/building-Building01/floor-Floor01/room-Room01/row-Row01/rack-Rack01.json"
+// {
+//   "geoRack": {
+//     "attributes": {
+//       "dn": "uni/fabric/site-Site01/building-Building01/floor-Floor01/room-Room01/row-Row01/rack-Rack01",
+//       "name": "Rack01",
+//       "rn": "rack-Rack01",
+//       "status": "created"
+//     },
+//     "children": []
+//   }
+// }
+//
+// // DELETE RACK
+// "api/node/mo/uni/fabric/site-Site01/building-Building01/floor-Floor01/room-Room01/row-Row01.json"
+// {
+//   "geoRow": {
+//     "attributes": {
+//       "dn": "uni/fabric/site-Site01/building-Building01/floor-Floor01/room-Room01/row-Row01",
+//       "status": "modified"
+//     },
+//     "children": [
+//       {
+//         "geoRack": {
+//           "attributes": {
+//             "dn": "uni/fabric/site-Site01/building-Building01/floor-Floor01/room-Room01/row-Row01/rack-Rack01",
+//             "status": "deleted"
+//           },
+//           "children": []
+//         }
+//       }
+//     ]
+//   }
+// }
+//
+// LIST RACKS
+// "api/node/mo/uni/fabric/site-Site01/building-Building01/floor-Floor01/room-Room01/row-Row01.json?query-target=children&target-subtree-class=geoRack"
+// response:
+// {
+//   "totalCount": "1",
+//   "imdata": [
+//     {
+//       "geoRack": {
+//         "attributes": {
+//           "childAction": "",
+//           "descr": "",
+//           "dn": "uni/fabric/site-Site01/building-Building01/floor-Floor01/room-Room01/row-Row01/rack-Rack01",
+//           "lcOwn": "local",
+//           "modTs": "2017-07-17T13:54:14.796+00:00",
+//           "monPolDn": "uni/fabric/monfab-default",
+//           "name": "Rack01",
+//           "status": "",
+//           "uid": "15374"
+//         }
+//       }
+//     }
+//   ]
+// }
