663f44fbde095d679048dfb4838a8e30b44d7500
Rob Phoenix <rob@robphoenix.com>
2017-08-10 15:34:38 +0100

    finish site.go

--- aci/site.go
+++ aci/site.go
@@ -103,6 +103,22 @@ type Site struct {
 	location
 }
 
+// NewSite instantiates a Site.
+func NewSite(name, description string) (*Site, error) {
+	site := Site{}
+	err := site.SetName(name)
+	if err != nil {
+		return &site, err
+	}
+	if description != "" {
+		err := site.SetDescription(description)
+		if err != nil {
+			return &site, err
+		}
+	}
+	return &site, nil
+}
+
 // Buildings ...
 func (site *Site) Buildings() []*Building {
 	var buildings []*Building
@@ -127,34 +143,178 @@ type Building struct {
 	location
 }
 
-// // Floors ...
-// func (building *Building) Floors() []*Floor {
-//         return building.locations
-// }
+// NewBuilding instantiates a Building.
+func NewBuilding(name, description string) (*Building, error) {
+	building := Building{}
+	err := building.SetName(name)
+	if err != nil {
+		return &building, err
+	}
+	if description != "" {
+		err := building.SetDescription(description)
+		if err != nil {
+			return &building, err
+		}
+	}
+	return &building, nil
+}
+
+// Floors ...
+func (building *Building) Floors() []*Floor {
+	var floors []*Floor
+	for _, location := range building.locations {
+		floors = append(floors, &Floor{*location})
+	}
+	return floors
+}
+
+// AddFloor ...
+func (building *Building) AddFloor(floor Floor) {
+	building.addLocation(floor.location)
+}
+
+// DeleteFloor ...
+func (building *Building) DeleteFloor(floor Floor) {
+	building.deleteLocation(floor.location)
+}
 
 // Floor ...
 type Floor struct {
-	Name        string
-	Description string
-	Rooms       []*Room
+	location
+}
+
+// NewFloor instantiates a Floor.
+func NewFloor(name, description string) (*Floor, error) {
+	floor := Floor{}
+	err := floor.SetName(name)
+	if err != nil {
+		return &floor, err
+	}
+	if description != "" {
+		err := floor.SetDescription(description)
+		if err != nil {
+			return &floor, err
+		}
+	}
+	return &floor, nil
+}
+
+// Rooms ...
+func (floor *Floor) Rooms() []*Room {
+	var rooms []*Room
+	for _, location := range floor.locations {
+		rooms = append(rooms, &Room{*location})
+	}
+	return rooms
+}
+
+// AddRoom ...
+func (floor *Floor) AddRoom(room Room) {
+	floor.addLocation(room.location)
+}
+
+// DeleteRoom ...
+func (floor *Floor) DeleteRoom(room Room) {
+	floor.deleteLocation(room.location)
 }
 
 // Room ...
 type Room struct {
-	Name        string
-	Description string
-	Rows        []*Row
+	location
+}
+
+// NewRoom instantiates a Room.
+func NewRoom(name, description string) (*Room, error) {
+	room := Room{}
+	err := room.SetName(name)
+	if err != nil {
+		return &room, err
+	}
+	if description != "" {
+		err := room.SetDescription(description)
+		if err != nil {
+			return &room, err
+		}
+	}
+	return &room, nil
+}
+
+// Rows ...
+func (room *Room) Rows() []*Row {
+	var rows []*Row
+	for _, location := range room.locations {
+		rows = append(rows, &Row{*location})
+	}
+	return rows
+}
+
+// AddRow ...
+func (room *Room) AddRow(row Row) {
+	room.addLocation(row.location)
+}
+
+// DeleteRow ...
+func (room *Room) DeleteRow(row Row) {
+	room.deleteLocation(row.location)
 }
 
 // Row ...
 type Row struct {
-	Name        string
-	Description string
-	Racks       []*Rack
+	location
+}
+
+// NewRow instantiates a Row.
+func NewRow(name, description string) (*Row, error) {
+	row := Row{}
+	err := row.SetName(name)
+	if err != nil {
+		return &row, err
+	}
+	if description != "" {
+		err := row.SetDescription(description)
+		if err != nil {
+			return &row, err
+		}
+	}
+	return &row, nil
+}
+
+// Racks ...
+func (row *Row) Racks() []*Rack {
+	var racks []*Rack
+	for _, location := range row.locations {
+		racks = append(racks, &Rack{*location})
+	}
+	return racks
+}
+
+// AddRack ...
+func (row *Row) AddRack(rack Rack) {
+	row.addLocation(rack.location)
+}
+
+// DeleteRack ...
+func (row *Row) DeleteRack(rack Rack) {
+	row.deleteLocation(rack.location)
 }
 
 // Rack ...
 type Rack struct {
-	Name        string
-	Description string
+	location
+}
+
+// NewRack instantiates a Rack.
+func NewRack(name, description string) (*Rack, error) {
+	rack := Rack{}
+	err := rack.SetName(name)
+	if err != nil {
+		return &rack, err
+	}
+	if description != "" {
+		err := rack.SetDescription(description)
+		if err != nil {
+			return &rack, err
+		}
+	}
+	return &rack, nil
 }
